c6c0624c39dd80ac6c5590b62e8287d6435b7fdc
박세훈 <psh3253@naver.com>
2023-07-19 16:55:48 +0900

    Feat: 백엔드 프로젝트 시작
    프로젝트 생성

---
+++ backend/HonJaRang/src/main/java/com/example/honjarang/HonJaRangApplication.java
@@ -0,0 +1,13 @@
+package com.example.honjarang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonJaRangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HonJaRangApplication.class, args);
+	}
+
+}
---
+++ backend/HonJaRang/src/test/java/com/example/honjarang/HonJaRangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.honjarang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HonJaRangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
