19b8b8cab41772f65a4930c3f53a8b9f40735430
박세훈 <psh3253@naver.com>
2023-07-20 23:46:09 +0900

    Test: 로그인 및 토큰 관련 테스트 코드 추가
    토큰 생성, 검증, 로그인 기능 등의 테스트 코드 추가

--- backend/HonJaRang/src/test/java/com/example/honjarang/security/service/TokenServiceTest.java
+++ backend/HonJaRang/src/test/java/com/example/honjarang/security/service/TokenServiceTest.java
@@ -2,111 +2,127 @@ package com.example.honjarang.security.service;
 
 import com.example.honjarang.domain.user.entity.Role;
 import com.example.honjarang.domain.user.entity.User;
+import com.example.honjarang.domain.user.repository.UserRepository;
 import com.example.honjarang.security.dto.TokenDto;
+import com.example.honjarang.security.exception.InvalidTokenException;
+import com.example.honjarang.security.exception.TokenExpiredException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.test.context.TestPropertySource;
 
 import java.util.Date;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
 
-@TestPropertySource("classpath:application-test.properties")
 @ExtendWith(MockitoExtension.class)
 class TokenServiceTest {
 
     @InjectMocks
     private TokenService tokenService;
 
-    @Value("${jwt.secret-key}")
-    private String SECRET_KEY;
+    @Mock
+    private UserRepository userRepository;
+
+    private static final String TEST_EMAIL = "test@test.com";
+    private static final Role TEST_ROLE = Role.ROLE_USER;
+    private static final String TEST_SECRET_KEY = "honjarang";
+
 
     @Test
-    @DisplayName("토큰 생성 테스트")
+    @DisplayName("토큰 생성")
     void generateToken() {
         // given
-        String email = "test@test.com";
-        Role role = Role.ROLE_USER;
 
         // when
-        TokenDto tokenDto = tokenService.generateToken(email, role);
+        TokenDto tokenDto = tokenService.generateToken(TEST_EMAIL, TEST_ROLE);
 
         // then
         assertThat(tokenDto).isNotNull();
     }
 
     @Test
-    @DisplayName("토큰 검증 테스트")
-    void verifyToken() {
-        // 토큰의 유효기간이 만료되지 않은 경우
-        {
-            // given
-            String email = "test@test.com";
-            Role role = Role.ROLE_USER;
+    @DisplayName("토큰 검증 성공")
+    void verifyToken_Success() {
+        // given
+        String token = Jwts.builder()
+                .setSubject(TEST_EMAIL)
+                .claim("role", TEST_ROLE)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 60 * 60 * 1000))
+                .signWith(SignatureAlgorithm.HS256, TEST_SECRET_KEY)
+                .compact();
 
-            String token = Jwts.builder()
-                    .setSubject(email)
-                    .claim("role", role)
-                    .setIssuedAt(new Date())
-                    .setExpiration(new Date(System.currentTimeMillis() + 60 * 60 * 1000))
-                    .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
-                    .compact();
+        // when
+        Boolean result = tokenService.verifyToken(token);
 
-            // when
-            Boolean result = tokenService.verifyToken(token);
+        // then
+        assertThat(result).isTrue();
+    }
 
-            // then
-            assertThat(result).isTrue();
-        }
-        // 토큰의 유효기간이 만료된 경우
-        {
-            // given
-            String email = "test@test.com";
-            Role role = Role.ROLE_USER;
+    @Test
+    @DisplayName("토큰 검증 실패 - 토큰이 만료된 경우")
+    void verifyToken_TokenExpiredException() {
+        // given
+        String token = Jwts.builder()
+                .setSubject(TEST_EMAIL)
+                .claim("role", TEST_ROLE)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() - 60 * 60 * 1000))
+                .signWith(SignatureAlgorithm.HS256, TEST_SECRET_KEY)
+                .compact();
 
-            String token = Jwts.builder()
-                    .setSubject(email)
-                    .claim("role", role)
-                    .setIssuedAt(new Date())
-                    .setExpiration(new Date(System.currentTimeMillis() - 60 * 60 * 1000))
-                    .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
-                    .compact();
+        // when & then
+        assertThrows(TokenExpiredException.class, () -> tokenService.verifyToken(token));
+    }
 
-            // when
-            Boolean result = tokenService.verifyToken(token);
+    @Test
+    @DisplayName("토큰 검증 실패 - 토큰이 유효하지 않은 경우")
+    void verifyToken_InvalidTokenException() {
+        // given
+        String token = Jwts.builder()
+                .setSubject(TEST_EMAIL)
+                .claim("role", TEST_ROLE)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 60 * 60 * 1000))
+                .signWith(SignatureAlgorithm.HS256, "invalidSecretKey")
+                .compact();
 
-            // then
-            assertThat(result).isFalse();
-        }
+        // when & then
+        assertThrows(InvalidTokenException.class, () -> tokenService.verifyToken(token));
     }
 
     @Test
     @DisplayName("토큰에서 사용자 정보 추출 테스트")
     void getUserByToken() {
         // given
-        String email = "test@test.com";
-        Role role = Role.ROLE_USER;
-
         String token = Jwts.builder()
-                .setSubject(email)
-                .claim("role", role)
+                .setSubject(TEST_EMAIL)
+                .claim("role", TEST_ROLE)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 60 * 60 * 1000))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .signWith(SignatureAlgorithm.HS256, TEST_SECRET_KEY)
                 .compact();
+
+        User expectedUser = User.builder()
+                .email(TEST_EMAIL)
+                .role(TEST_ROLE)
+                .build();
+        given(userRepository.findByEmail(TEST_EMAIL)).willReturn(Optional.of(expectedUser));
 
         // when
         User user = tokenService.getUserByToken(token);
 
         // then
         assertThat(user).isNotNull();
-        assertThat(user.getEmail()).isEqualTo(email);
-        assertThat(user.getRole()).isEqualTo(role);
+        assertThat(user.getEmail()).isEqualTo(TEST_EMAIL);
+        assertThat(user.getRole()).isEqualTo(TEST_ROLE);
     }
 }
