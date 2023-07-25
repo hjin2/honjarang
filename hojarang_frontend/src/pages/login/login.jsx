d56f2c6dd60bb1ecd2637a82d8e31266e3b6b0bb
hjhj-kk <hyeonji.k128@gmail.com>
2023-07-25 17:59:32 +0900

    Design: Login page 미완성

--- hojarang_frontend/src/pages/login/login.jsx
+++ hojarang_frontend/src/pages/login/login.jsx
@@ -1,9 +1,34 @@
-// import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 export default function login() {
   return (
-    <div>
-        <h1>Login</h1>
+    <div style={{
+      justifyContent: 'center', 
+      width: '100%', height: '100vh'
+    }}>
+      <form >
+        <label htmlFor="email">이메일</label>
+        <input type="email" />
+        <label htmlFor="password">비밀번호</label>
+        <input type="empassword" />
+      </form>
+      <button>로그인</button>
+      <div>
+        <Link to='/'>비밀번호 찾기</Link> | <Link to='/signup'>회원가입</Link>
+      </div>
+      <div>
+        <span>카카오로 시작하기</span>
+      </div>
+      <div>
+        <span>네이버로 시작하기</span>
+      </div>
+      <div>
+        <span>구글로 시작하기</span>
+      </div>
+
+
     </div>
   )
 }
