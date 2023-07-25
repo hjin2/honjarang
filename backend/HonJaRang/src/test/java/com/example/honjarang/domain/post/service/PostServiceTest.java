929e0c47426cb36bb3bf665190e4001723d15a5b
차재호 <ischar@naver.com>
2023-07-26 06:50:28 +0900

    Test: 게시글 삭제 단위 테스트 추가
    - 게시글 삭제 단위 테스트 추가
      - 게시글 작성 성공했을 경우
      - 게시글이 존재하지 않을 경우
      - 작성자가 다를 경우

--- backend/HonJaRang/src/test/java/com/example/honjarang/domain/post/service/PostServiceTest.java
+++ backend/HonJaRang/src/test/java/com/example/honjarang/domain/post/service/PostServiceTest.java
@@ -3,24 +3,35 @@ package com.example.honjarang.domain.post.service;
 
 import com.example.honjarang.domain.post.dto.PostCreateDto;
 import com.example.honjarang.domain.post.entity.Post;
-import com.example.honjarang.domain.post.exception.ContentEmptyException;
-import com.example.honjarang.domain.post.exception.TitleAndContentEmptyException;
-import com.example.honjarang.domain.post.exception.TitleEmptyException;
+import com.example.honjarang.domain.post.exception.*;
 import com.example.honjarang.domain.post.repository.PostRepository;
 import com.example.honjarang.domain.user.entity.User;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.test.context.support.WithMockUser;
 
+import java.util.Objects;
+import java.util.Optional;
+
+import static com.amazonaws.services.simpleemail.model.TlsPolicy.Optional;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
 public class PostServiceTest {
 
+    @Spy
     @InjectMocks
     private PostService postService;
 
@@ -121,6 +132,64 @@ public class PostServiceTest {
 
         // when & then
         assertThrows(TitleAndContentEmptyException.class, () -> postService.createPost(postCreateDto,user));
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("게시글 삭제 성공")
+    void deletePost_Success() {
+
+        // given
+        Long postId = 1L;
+        User user = User.builder()
+                .email(TEST_EMAIL)
+                .password(TEST_PASSWORD)
+                .build();
+
+        // when & then
+        postRepository.deleteById(postId);
+    }
+
+
+    @Test
+    @DisplayName("게시글 삭제 실패 - 게시글이 존재하지 않을 경우")
+    void deletePost_EmptyPostException() {
+
+        // given
+        Long postId = 1L;
+        User user = User.builder()
+                .email(TEST_EMAIL)
+                .password(TEST_PASSWORD)
+                .build();
+
+        // when & then
+        assertThrows(PostNotFoundException.class, () -> postService.deletePost(postId, user));
+    }
+
+    @Test
+    @DisplayName("게시글 삭제 실패 - 작성자가 다를 경우")
+    void deletePost_DisMatchUserException() {
+
+        // given
+        User user = User.builder()
+                .email(TEST_EMAIL)
+                .password(TEST_PASSWORD)
+                .build();
+        Long postId = 1L;
+        Post post = Post.builder()
+                .title(TEST_TITLE)
+                .content(TEST_CONTENT)
+                .user(user)
+                .build();
+
+        User invalidUser = User.builder()
+                .email("234234")
+                 .password(TEST_PASSWORD)
+                .build();
+
+        doThrow(new InvalidUserException("사용자가 다릅니다.")).when(postService).deletePost(postId, invalidUser);
+        // when
+        assertThrows(InvalidUserException.class, () -> postService.deletePost(postId, invalidUser));
     }
 
 
