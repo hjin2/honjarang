b396348913499f55b5e48393a670d6bab27db749
Jaeho Cha <brownhare0221@gmail.com>
2023-07-25 14:48:27 +0900

    feat: 게시글 삭제 기능 추가
    - 게시글 삭제 기능 추가

--- backend/HonJaRang/src/main/java/com/example/honjarang/domain/post/service/PostService.java
+++ backend/HonJaRang/src/main/java/com/example/honjarang/domain/post/service/PostService.java
@@ -2,14 +2,15 @@ package com.example.honjarang.domain.post.service;
 
 
 import com.example.honjarang.domain.post.dto.PostCreateDto;
-import com.example.honjarang.domain.post.exception.ContentEmptyException;
-import com.example.honjarang.domain.post.exception.TitleAndContentEmptyException;
-import com.example.honjarang.domain.post.exception.TitleEmptyException;
+import com.example.honjarang.domain.post.entity.Post;
+import com.example.honjarang.domain.post.exception.*;
 import com.example.honjarang.domain.post.repository.PostRepository;
 import com.example.honjarang.domain.user.entity.User;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -33,4 +34,14 @@ public class PostService {
         return postRepository.save(postCreateDto.toEntity(user)).getId();
     }
 
+    @Transactional
+    public void deletePost(long id, User user) {
+        Post post = postRepository.findById(id).orElseThrow(() ->
+                new PostNotFoundException("존재하지 않는 게시글입니다."));
+
+        if (!Objects.equals(post.getUser().getId(),user.getId())) {
+            throw new InvalidUserException("작성자만 삭제할 수 있습니다.");
+        }
+        postRepository.deleteById(id);
+    }
 }
