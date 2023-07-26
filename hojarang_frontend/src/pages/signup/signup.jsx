3445ba1d96b730dcfd67aee2b758a2cbc6842f17
김동혁 <fhg4561@naver.com>
2023-07-26 09:11:27 +0900

    Design : 회원가입 페이지 디자인

--- hojarang_frontend/src/pages/signup/signup.jsx
+++ hojarang_frontend/src/pages/signup/signup.jsx
@@ -1,7 +1,49 @@
+import './signup.css'
+
 export default function Signup() {
+  const duplicate_email = false
+  const duplicate_nickname = false
   return (
     <div>
         <h1>Signup</h1>
+          <div class="email">
+            이메일
+            <br />
+            <input type="text" />
+            <button>인증번호 전송</button>
+          </div>
+          <div class="verify">
+            인증번호 입력
+            <br />
+            <input type="number" />
+            <button>인증번호 확인</button>
+          </div>
+          <div class="nickname">
+            닉네임
+            <br />
+            <input type="text" />
+            <button>닉네임 중복 확인</button>
+          </div>
+          <div class="password">
+            비밀번호
+            <br />
+            <input type="password" id="password"/>
+          </div>
+          <div class="password_confirm">
+            비밀번호 확인
+            <br />
+            <input type="password" id="password_confirm"/>
+          </div>
+          <div class="address">
+            주소
+            <br />
+            <input type="text" /> <button>주소 검색</button>
+          </div>
+          <div class="address2">
+            상세주소
+            <br />
+            <input type="text" id="address2"/>
+          </div>
     </div>
   )
 }
