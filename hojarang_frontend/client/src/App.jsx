8b84bb963c92709aac4f86b32fb2384994be1b80
김동혁 <fhg4561@naver.com>
2023-07-31 15:32:19 +0900

    Fix : APP.jsx Route 경로 수정 / 추가

--- hojarang_frontend/client/src/App.jsx
+++ hojarang_frontend/client/src/App.jsx
@@ -12,33 +12,45 @@ import Header from './components/Header/Header';
 import Market from './pages/Market/Market';
 import WebRTC from './pages/WebRTC/WebRTC';
 import Board from './pages/Board/Board';
-import Chatting from './pages/Chatting/Chatting';
 import Map from './pages/Map/Map';
 import MyPage from './pages/MyPage/MyPage';
 import FindPassword from './pages/FindPassword/FindPassword';
 import ChangePassword from './pages/FindPassword/ChangePassword';
+import ArticleCreate from './pages/Board/ArticleCreate';
+import { ArticleDetail } from './pages/Board/ArticleDetail';
+import Checkout from './pages/Checkout/Checkout';
+import Fail from './pages/Checkout/Fail';
+import Success from './pages/Checkout/Success';
+import DefaultLayout from './components/DefaultLayout';
 import DeliveryDetail from './components/Market/DeliveryDetail';
 
 
 function App() {
   return (
-    <div className='App'>
+    <div className='app'>
       <BrowserRouter>
         <Header/>
         <Routes>
-          <Route exact path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />}/>
-          <Route path="/webrtc" element={<WebRTC />}/>
-          <Route path="/chatting" element={<Chatting />}/>
-          <Route path="/market" element={<Market />}/>
-          <Route path="/board" element={<Board />}/>
-          <Route path="/map" element={<Map />}/>
-          <Route path="/market/deliverydetail/:id" element={<DeliveryDetail/>}></Route>
-          <Route path="/mypage/:nickname" element={<MyPage />}/>
-          <Route path="/findpassword" element={<FindPassword/>}/>
-          <Route path="/findpassword/changepassword" element={<ChangePassword/>}></Route>
-          <Route path="/*" element={<Notfound />}/>
+          <Route element={ <DefaultLayout/> }>
+            <Route exact path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />}/>
+            <Route path="/webrtc" element={<WebRTC />}/>
+            <Route path="/chatting" element={<Chatting />}/>
+            <Route path="/market" element={<Market />}/>
+            <Route path="/market/deliverydetail/:id" element={<DeliveryDetail/>}></Route>
+            <Route path="/board" element={<Board />}/>
+            <Route path="/map" element={<Map />}/>
+            <Route path="/mypage/:nickname" element={<MyPage />}/>
+            <Route path="/findpassword" element={<FindPassword/>}/>
+            <Route path="/findpassword/changepassword" element={<ChangePassword/>}></Route>
+            <Route path="/board/articlecreate" element={<ArticleCreate/>}></Route>
+            <Route path="/board/article/:id" element={<ArticleDetail />} />
+            <Route path="/*" element={<Notfound />}/>
+            <Route path="/checkout" element={ <Checkout/> }/>
+            <Route path="/checkout/fail" element={ <Fail/> }/>
+            <Route path="/checkout/success" element={ <Success/> }/>
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
