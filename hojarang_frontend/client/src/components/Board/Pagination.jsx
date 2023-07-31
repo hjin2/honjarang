73b4f293faacf9f3c99cd640ad610a9c53a911f3
김동혁 <fhg4561@naver.com>
2023-07-31 10:25:40 +0900

    Feat : 게시판 목록 조회 기능 추가

---
+++ hojarang_frontend/client/src/components/Board/Pagination.jsx
@@ -0,0 +1,34 @@
+
+
+function Pagination({ total, limit, page, setPage }) {
+  const numPages = Math.ceil(total / limit);
+
+  return (
+    <div>
+      <nav className="flex justify-center items-center gap-4 m-16">
+        <button onClick={() => setPage(page - 1)} disabled={page === 1}>
+          &lt;
+        </button>
+        {Array(numPages)
+          .fill()
+          .map((_, i) => (
+            <button
+              key={i + 1}
+              onClick={() => setPage(i + 1)}
+              aria-current={page === i + 1 ? "page" : null}
+            >
+              {i + 1}
+            </button>
+          ))}
+        <buitton onClick={() => setPage(page + 1)} disabled={page === numPages}>
+          &gt;
+        </buitton>
+      </nav>
+    </div>
+  );
+}
+
+const [cate, setCate] = useState('')
+
+
+export default Pagination;
