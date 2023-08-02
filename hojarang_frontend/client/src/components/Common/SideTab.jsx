e651424405fdec1c3da33c12a1a71edf124ecaa9
jaewook <jaewook71828@gmail.com>
2023-08-02 15:31:28 +0900

    [FE] FEAT : 공동 component 구현

--- hojarang_frontend/client/src/components/Common/SideTab.jsx
+++ hojarang_frontend/client/src/components/Common/SideTab.jsx
@@ -1,17 +1,23 @@
 import React, { useState } from 'react'
 import Content from './Content'
 
-export default function SideTab(props){
-  const tabs = props.tabs
-  const [activeTabIndex, setActiveTabIndex] = useState(0)
+export default function SideTab({ tabs, activeTabIndex, setActiveTabIndex }){
 
   return (
-    <div>
-      <Content
-        tabs = {tabs}
-        activeTabIndex = {activeTabIndex}
-        setActiveTabIndex = {setActiveTabIndex}
-      />
+    <div className='space-x-14 mx-auto'>
+      <ul className="space-y-5">
+        {tabs.map((tab, index) => (
+          <li
+            key={index}
+            onClick={() => setActiveTabIndex(index)}
+            className="m-auto cursor-pointer"
+          >
+            <div className={`${activeTabIndex === index ? 'font-bold' : ''}`}>
+              {tab.title}
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
