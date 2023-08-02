a177dc9c79774f4c752b045dfd3652b243e45610
hjhj-kk <hyeonji.k128@gmail.com>
2023-08-02 14:00:43 +0900

    Feat: 장터페이지

--- hojarang_frontend/client/src/components/Market/SideBar.jsx
+++
@@ -1,3 +0,0 @@
-export default function SideBar() {
-  return <input type="text" />;
-}
---
+++ hojarang_frontend/client/src/components/Market/Transaction.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Transaction = () => {
+  return (
+    <div>Transaction</div>
+  )
+}
