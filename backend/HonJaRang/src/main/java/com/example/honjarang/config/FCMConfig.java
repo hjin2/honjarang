65080647d2f5b9c41b334a672f1a905000df7f3d
박세훈 <psh3253@naver.com>
2023-08-07 09:39:49 +0900

    Feat: Token Dto 수정 및 충돌 해결
    - TokenDto 수정 및 충돌 문제 해결

--- backend/HonJaRang/src/main/java/com/example/honjarang/config/FCMConfig.java
+++ backend/HonJaRang/src/main/java/com/example/honjarang/config/FCMConfig.java
@@ -9,9 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Base64;
 
 @Configuration
 public class FCMConfig {
@@ -20,7 +18,7 @@ public class FCMConfig {
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        FirebaseOptions options = new FirebaseOptions.Builder()
+        FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(fireBaseAdminJson.getBytes())))
                 .build();
         return FirebaseApp.initializeApp(options);
--- backend/HonJaRang/src/main/java/com/example/honjarang/domain/user/controller/UserController.java
+++ backend/HonJaRang/src/main/java/com/example/honjarang/domain/user/controller/UserController.java
@@ -33,7 +33,7 @@ public class UserController {
     @PostMapping("/login")
     public ResponseEntity<TokenDto> login(@RequestBody LoginDto loginDto) {
         User user = userService.login(loginDto);
-        TokenDto tokenDto = tokenService.generateToken(user.getEmail(), user.getRole());
+        TokenDto tokenDto = tokenService.generateToken(user.getId(), user.getEmail(), user.getRole());
         return ResponseEntity.ok(tokenDto);
     }
 
