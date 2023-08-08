efe4fff9429b9f94ff91919a0f74ac26089b479d
hjhj-kk <hyeonji.k128@gmail.com>
2023-08-08 17:49:14 +0900

    Feat: 공동배달 생성 및 상세페이지

--- hojarang_frontend/client/src/components/Market/DeliveryDetail.jsx
+++ hojarang_frontend/client/src/components/Market/DeliveryDetail.jsx
@@ -1,68 +1,75 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Cart from './Cart';
+import Modal from '../Common/Modal';
+import axios from 'axios';
 
 function DeliveryDetail() {
-  const params = useParams();
+
+  const {id} = useParams();
+  const token = localStorage.getItem("access_token")
+  const URL = import.meta.env.VITE_APP_API
+  const headers = {'Authorization': `Bearer ${token}`};
+
   // 공동 배달 상세
-  const [detail, setDetail] = useState({
-    id: 1,
-    content: '맛있는 치킨',
-    delivery_charge: '5000',
-    current_total_price: '5000',
-    target_min_price: '30000',
-    deadline: '2023-08-09',
-    store_id: '1234',
-    store_name: 'BBQ 인동점',
-    store_image: '/src/assets/panda-bear.png',
-    user_id: '2',
-    nickname: 'ischar',
-    created_at: '2023-08-07'
-  });
+  const [detail, setDetail] = useState([]);
+  useEffect(() => {
+    axios.get(`${URL}/api/v1/joint-deliveries/${id}`,  {headers})
+    .then((res) => {
+      setDetail(res.data)
+      console.log(res.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }, [id]);
 
   // 메뉴 목록 조회
-  const [menu, setMenu] = useState([
-    {id: 1,
-    name: '황금올리브',
-    price: '20000',
-    image: '/src/assets/panda-bear.png',
-    store_id: 1},
-    {id: 2,
-    name: '반반',
-    price: '22000',
-    image: '/src/assets/panda-bear.png',
-    store_id: 1},
-    {id: 3,
-    name: '자메이카',
-    price: '190000',
-    image: '/src/assets/panda-bear.png',
-    store_id: 1},
-    ])
+  const [menu, setMenu] = useState([]);
+  useEffect(() => {
+    axios.get(`${URL}/api/v1/joint-deliveries/${id}/menus`, {headers})
+    .then((res) => {
+      setMenu(res.data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  })
 
-    // 장바구니에 추가된 메뉴들
-    const [cart, setCart] = useState([])
+  // if (!detail) {
+  //   return <div>Loading</div>
+  // }
 
-    // 장바구니에 메뉴 추가
-    const addToCart = (menuItem) => {
-      // 메뉴 중복 확인
-      const isMenuInCart = cart.some((item) => item.id === menuItem.id)
-      if(!isMenuInCart) {
-        setCart([...cart, menuItem])
-      }
+  // 장바구니에 추가된 메뉴들
+  const [cart, setCart] = useState([])
 
+  // 메뉴 담기
+  const addToCart = (menuItem) => {
+    // 메뉴 중복 확인
+    const isMenuInCart = cart.some((item) => item.id === menuItem.id)
+    if(!isMenuInCart) {
+      setCart([...cart, menuItem])
     }
+  }
 
-  // useEffect(() => {
-  //   fetch(`http://localhost8080://api/v1/joint-delivery/${params.id}`)
-  //     .then((res) => res.json())
-  //     .then((data) => setDetail(data));
-  // });
+  // 장바구니에 메뉴 담아서 보여주는 모달
+  const [modalState, setModalState] = useState(false);
+  const onModalOpen = (menuItem) => {
+    addToCart(menuItem)
+    setModalState(!modalState);
+  };
 
- 
+  const [selectedMenu, setSelectedMenu] = useState('')
+  const menuInCart = (cart) => {
+    setSelectedMenu(cart)
+    setModalState(false)
+  }
+
   return (
     <div>
       <div className="border rounded-lg max-w-2xl mx-auto mt-10 pb-3 p-5 space-y-5 ">
         <div className="flex justify-between">
+          
           <div>
             <div className="font-bold text-3xl flex items-end">{detail.store_name}</div>
           </div>
@@ -72,21 +79,34 @@ function DeliveryDetail() {
           </div>
         </div>
         <hr />
+        <div className="m-2">
+          {detail.content}
+        </div>
+        <hr />
         {/* 메뉴 목록 */}
-        <div>
+        <div className="menu-container overflow-y-scroll max-h-96 border rounded-md px-4">
           {menu.map((menuItem) => (
-            <div key={menuItem.id} className="flex justify-between items-center space-x-4">
+            <div key={menuItem.id} className="flex justify-between items-center space-x-4 my-2">
               <img src={menuItem.image} alt={menuItem.name} className="w-16 h-16" />
-              <div>
+              <div className="text-center">
                 <p className="font-semibold">{menuItem.name}</p>
                 <p>가격: {menuItem.price}</p>
               </div>
               <div>
-                <button onClick={() => addToCart(menuItem)}>
+                {/* <button onClick={() => addToCart(menuItem)}> */}
+                <button onClick={onModalOpen}>
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0l-3-3m3 3l3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
                   </svg>
                 </button>
+                {modalState &&  (
+                  <Modal modalState={modalState} setModalState={setModalState}>
+                    <Cart 
+                    modalState={modalState} 
+                    setModalState={setModalState} 
+                    cartItems={menuInCart} />
+                  </Modal>
+                )}
               </div>
             </div>
           ))}
@@ -99,7 +119,7 @@ function DeliveryDetail() {
         <p>{detail.current_total_price}/{detail.target_min_price}</p>
         {/* 메뉴를 담으면 장바구니 버튼 활성화 */}
         {cart.length > 0 && (<button>장바구니</button>)} 
-        <Cart cartItems={cart} />
+        {/* <Cart cartItems={cart} /> */}
       </div>
     </div>
     
--- hojarang_frontend/client/src/pages/Market/DeliveryCreate.jsx
+++ hojarang_frontend/client/src/pages/Market/DeliveryCreate.jsx
@@ -7,6 +7,9 @@ import { useNavigate } from 'react-router-dom';
 
 
 export default function DeliveryCreate() {
+
+  const token = localStorage.getItem("access_token")
+  const URL = import.meta.env.VITE_APP_API
   
   // 가게 목록 모달
   const [modalState, setModalState] = useState(false);
@@ -21,72 +24,80 @@ export default function DeliveryCreate() {
   const [targetMinPrice, setTargetMinPrice] = useState('')
   const [deadline, setDeadline] = useState('')
 
+  const [selectedStore, setSelectedStore] = useState('')
   // 선택한 가게 ID 저장
-  const onStoreClick = (storeId) => {
-    setStoreId(storeId)
+  const onStoreClick = (store) => {
+    setSelectedStore(store)
+    setStoreId(store.id)
     setModalState(false)
   }
 
-  // 마감시간 최대 30분 값 검증 및 초과하면 값 수정하도록
-  const maxDeadline = 30
-  const handleChange = (e) => {
-    const newDeadline = parseInt(e.target.value)
-    if (newDeadline > maxDeadline) {
-      setDeadline(maxDeadline)
-    }else {
-      setDeadline(newDeadline)
-    }
+  // 선택한 가게 전달
+  const handleStoreClick = (store) => {
+    onStoreClick(store);
+    setModalState(false)
   }
 
+  // 하는 중......
+  // 마감시간 최대 1시간, 초과하면 값 수정하도록
+  // const maxDeadline = new Date();
+  // maxDeadline.setHours(maxDeadline.getHours() + 1);
+  
+  // const deadlineChange = (e) => {
+  //   const selectedDeadline = new Date(e.target.value);
+  //   console.log(selectedDeadline)
+  //   console.log(maxDeadline)
+  //   if (selectedDeadline <= maxDeadline) {
+  //     const formattedDeadline = selectedDeadline.toISOString().slice(0,10) + " " + selectedDeadline.toISOString().slice(11, 16) + ":00";
+  //     setDeadline(formattedDeadline);
+  //     // console.log(selectedDeadline.toISOString().slice(0,10) + " " + selectedDeadline.slice(16,24))
+  //   }
+  //   else{
+  //     setDeadline(maxDeadline);
+  //   }
+  // };
+  
+
   const navigate = useNavigate()
-  const goDeliveryDetail = () => {
-    navigate('market/deliverydetail/:id')
-  }
   // axios
   const createDelivery = () => {
- 
-    const headers = {
-      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJpc2NoYXJAbmF2ZXIuY29tIiwicm9sZSI6IlJPTEVfQURNSU4iLCJpYXQiOjE2OTEzOTE1MDAsImV4cCI6MTY5MTM5NTEwMH0.MKsMKUfqGED3VQZ7_acMqQoiMAJSyRvATLQch6DTPEw'
-    };
+
+    const headers = {'Authorization': `Bearer ${token}`};
     const data = {
       content: content,
       store_id: storeId,
       delivery_charge: deliveryCharge,
       target_min_price: targetMinPrice,
-      deadline: deadline
+      deadline: deadline.slice(0,10)+" "+deadline.slice(11,16)+":00"
     };
 
-    axios.post('http://honjarang.kro.kr:30000/api/v1/joint-deliveries', data, {headers})
-    .then((res) => {
-      console.log(res.data)
-      goDeliveryDetail()
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+    console.log(data)
+    axios.post(`${URL}/api/v1/joint-deliveries`, data, {headers})
+      .then((res) => {
+        console.log(res.data)
+        navigate(`/market/deliverydetail/${res.data}`)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   };
 
 
   return (
     <div className="border rounded-lg max-w-2xl mx-auto mt-10 pb-3 p-5 space-y-5 ">
-      {/* <div>
-        <div>가게 검색</div>
-        <input type="text" />
-        <div type="button" onClick={onModalOpen}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-          </svg>
-        </div>
-      </div> */}
-
       <div>
         <div>가게</div>
-        <input type="number" value={storeId} onChange={(e) => setStoreId(e.target.value)} onClick={onModalOpen}/>
+        <input type="number" value={selectedStore ? selectedStore.id : ''} readOnly onClick={onModalOpen}/>
           {modalState && (
             <Modal modalState={modalState} setModalState={setModalState}>
-              <Stores modalState={modalState} setModalState={setModalState}/>
+              <Stores 
+              modalState={modalState} 
+              setModalState={setModalState}
+              onStoreClick={handleStoreClick}/>
             </Modal>
           )}
+        {/* <input type="number" value={storeId} 
+        onChange={(e) => setStoreId(e.target.value)}/> */}
       </div>
       <div>
         <div>내용</div>
@@ -107,42 +118,12 @@ export default function DeliveryCreate() {
         onChange={(e) => setDeliveryCharge(e.target.value)}/>
       </div>
       <div>
-        <div> 마감시간(초까지 입력) </div>
-        {/* <input type="datetime" value={deadline} 
-        onChange={handleChange}/> */}
+        <div> 마감시간(최대 1시간) </div>
+        {/* <input type="datetime-local" value={deadline} 
+        onChange={deadlineChange}/> */}
         <input type="datetime-local" value={deadline} 
         onChange={(e) => setDeadline(e.target.value)}/>
       </div>
-      {/* <div>
-        <div>메뉴선택</div>
-        <form action="" value="메뉴선택">
-          <select name="" id="" className="border">
-            <option value="황금올리브치킨">황금올리브치킨</option>
-            <option value="양념치킨">양념치킨</option>
-            <option value="자메이카치킨">자메이카치킨</option>
-            <option value="황올반+양념반">황올반+양념반</option>
-          </select>
-        </form>
-      </div> */}
-      {/* <div>
-        <div>장바구니</div>
-        <div className="border rounded-md mx-auto">
-          <div className="border-0">
-            <div className="w-full flex">
-              <div className="w-1/6"><img src="" alt="" />이미지</div>
-              <div className="w-4/6">황금올리브치킨</div>
-              <div className="w-1/6">20,000원</div>
-            </div>
-          </div>
-          <div className="border-0">
-            <div className="w-full flex">
-              <div className="w-1/6"><img src="" alt="" />이미지</div>
-              <div className="w-4/6">황금올리브치킨</div>
-              <div className="w-1/6">20,000원</div>
-            </div>
-          </div>
-        </div>
-      </div> */}
       <div>
         <button type="button" className="main1-full-button w-20" 
         onClick={createDelivery}>작성완료</button>
