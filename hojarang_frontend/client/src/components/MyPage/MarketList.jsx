c287d751b734822cba6ba75b20d0f477b4af3f8e
jaewook <jaewook71828@gmail.com>
2023-08-09 16:20:04 +0900

    [FE] 연습

--- hojarang_frontend/client/src/components/MyPage/MarketList.jsx
+++ hojarang_frontend/client/src/components/MyPage/MarketList.jsx
@@ -1,3 +1,12 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 export default function MarketList() {
+  const URL = import.meta.env.VITE_APP_API
+  const token = localStorage.getItem("access_token")
+  useEffect(()=>{
+    axios.get(`${URL}/api/v1/users/joint-deliveries-participating`)
+  })
+
   return <div>MarketList</div>;
 }
