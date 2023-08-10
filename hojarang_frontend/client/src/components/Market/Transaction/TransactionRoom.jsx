a1f94dd32ac59984c0d7543d9938a35fd5369df0
jaewook <jaewook71828@gmail.com>
2023-08-10 19:56:21 +0900

    [FE] DESIGN : 공동구매 상세페이지 디자인

--- hojarang_frontend/client/src/components/Market/Transaction/TransactionRoom.jsx
+++ hojarang_frontend/client/src/components/Market/Transaction/TransactionRoom.jsx
@@ -12,14 +12,14 @@ export default function TransactionRoom(roomData) {
         {/* 이미지와 그 위 텍스트 */}
         <div className="flex justify-center relative">
           <div className="w-32 h-32">
-            <img src={`https://honjarang-bucket.s3.ap-northeast-2.amazonaws.com/transactionImage/${transaction_image}`} alt="상품 이미지" className="w-full h-full" />
+            <img src={`https://honjarang-bucket.s3.ap-northeast-2.amazonaws.com/transactionImage/${transaction_image}`} alt="상품 이미지" className="w-full h-full rounded-lg" />
           </div>
           <div className="absolute inset-0 flex items-end justify-end">
             <p className="text-sm font-bold">{is_complete}</p>
           </div>
         </div>
         {/* 상품 제목 */}
-        <div className="flex justify-between my-1">
+        <div className="flex justify-between my-4">
           <div>{title}</div>
           <div>{price} </div>
         </div>
