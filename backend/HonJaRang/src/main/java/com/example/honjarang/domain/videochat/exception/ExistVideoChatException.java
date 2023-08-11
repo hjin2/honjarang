edb2b3bc2b3816615e7fa5ba5255649ce2255ae1
차재호 <ischar@naver.com>
2023-08-11 16:21:25 +0900

    Revert "Merge branch 'BE_화상_채팅_기능_추가' into 'backend'"
    This reverts merge request !141

--- backend/HonJaRang/src/main/java/com/example/honjarang/domain/videochat/exception/ExistVideoChatException.java
+++
@@ -1,7 +0,0 @@
-package com.example.honjarang.domain.videochat.exception;
-
-public class ExistVideoChatException extends RuntimeException {
-    public ExistVideoChatException(String message) {
-        super(message);
-    }
-}
