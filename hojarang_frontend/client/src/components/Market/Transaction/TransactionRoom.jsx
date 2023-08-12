56384000a86b190c2c07049bca11a11a2edb914f
jaewook <jaewook71828@gmail.com>
2023-08-12 20:40:38 +0900

    [FE] FEAT : 이미지 없을때 기본이미지 적용

--- hojarang_frontend/client/src/components/Market/Transaction/TransactionRoom.jsx
+++ hojarang_frontend/client/src/components/Market/Transaction/TransactionRoom.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router";
+import defaultImage from "@/assets/noimage.png"
 
 export default function TransactionRoom(roomData) {
   const navigate = useNavigate()
@@ -8,12 +9,19 @@ export default function TransactionRoom(roomData) {
   }
   return (
     <div >
-      <div className="border-2 border-gray1 w-52 h-60 p-2 rounded-lg">
+      <div className="border-2 border-gray1 w-52 p-2 rounded-lg">
         {/* 이미지와 그 위 텍스트 */}
         <div className="flex justify-center">
-          <div className="w-32 h-32">
-            <img src={`https://honjarang-bucket.s3.ap-northeast-2.amazonaws.com/transactionImage/${transaction_image}`} alt="상품 이미지" className="w-full h-full rounded-lg" />
-          </div>
+          {transaction_image ? (
+            <div className="w-32 h-32">
+              <img src={`https://honjarang-bucket.s3.ap-northeast-2.amazonaws.com/transactionImage/${transaction_image}`} alt="상품 이미지" className="w-full h-full rounded-lg" />
+            </div>
+          ):(
+            <div className="w-32 h-32">
+              <img src={defaultImage} alt="기본이미지" />
+            </div>
+          )}
+
         </div>
         {/* 상품 제목 */}
         <div className="flex justify-between my-4">
