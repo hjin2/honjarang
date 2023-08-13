96ed24cb9ed307dbf46640fb8e77ed34a3ede32e
jaewook <jaewook71828@gmail.com>
2023-08-13 16:49:14 +0900

    [FE] webrtc 수정

--- hojarang_frontend/client/src/components/WebRTC/Util.jsx
+++ hojarang_frontend/client/src/components/WebRTC/Util.jsx
@@ -13,7 +13,7 @@ export const createSession = async (sessionId) => {
 };
 
 export const createToken = async (sessionId) => {
-	const response = await axios.post(APPLICATION_SERVER_URL + 'api/sessions/' + sessionId + '/connections', {}, {
+	const response = await axios.post(APPLICATION_SERVER_URL + '/api/sessions/' + sessionId + '/connections', {}, {
 		headers: { 'Content-Type': 'application/json', },
 	});
 	return response.data; // The token
