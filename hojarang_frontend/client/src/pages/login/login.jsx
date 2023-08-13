f761f7ff105847c9683ab5651c8afc6894be61c3
hjhj-kk <hyeonji.k128@gmail.com>
2023-08-13 14:42:46 +0900

    Design: 로그인

--- hojarang_frontend/client/src/pages/login/login.jsx
+++ hojarang_frontend/client/src/pages/login/login.jsx
@@ -37,38 +37,38 @@ export default function Login() {
 
 
   return (
-    <div className="container flex flex-col items-center justify-center mx-auto ">
+    <div className="container flex flex-col items-center justify-center mx-auto mt-10">
       <form className="space-y-4 md:space-y-6" action="#">
         <div>
-          <label htmlFor="email" className="block mb-2">
+          <label htmlFor="email" className="block mb-1 font-semibold text-lg text-main2">
             이메일
           </label>
           <input
             type="text"
-            className="border-gray3 rounded-lg block focus:outline-main2 focus:outline-2"
+            className="border-gray2 rounded-lg block w-72 h-10 text-base p-2 focus:outline-main2"
             onChange = {e => setEmail(e.target.value)}/>
         </div>
         <div>
-          <label htmlFor="password" className="block mb-2">
+          <label htmlFor="password" className="block mb-1 font-semibold text-lg text-main2">
             비밀번호
           </label>
           <input
             type="password"
-            className="border-gray3 rounded-lg block focus:outline-main2 focus:outline-2"
+            className="border-gray2 rounded-lg block w-72 h-10 text-base p-2 focus:outline-main2 "
             onChange = {e => setPwd(e.target.value)}/>
         </div>
       </form>
       <button
         onClick={login}
-        className="w-32 main1-button my-5"
+        className="w-72 h-10 main1-button my-10"
       >
         로그인
       </button>
-      <div>
-        <Link to="/findpassword">비밀번호 찾기</Link> |{' '}
+      <div className="w-72 flex justify-around text-base p-1 text-gray4">
         <Link to="/signup">회원가입</Link>
+        <Link to="/findpassword">비밀번호 찾기</Link> 
       </div>
-      <div>
+      {/* <div>
         <span>카카오로 시작하기</span>
       </div>
       <div>
@@ -76,7 +76,7 @@ export default function Login() {
       </div>
       <div>
         <span>구글로 시작하기</span>
-      </div>
+      </div> */}
     </div>
   );
 }
