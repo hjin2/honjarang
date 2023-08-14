ef019b514bafcf4dccfcee64432e9273ac39ed21
jaewook <jaewook71828@gmail.com>
2023-08-14 09:48:06 +0900

    [FE] DESIGN : 게시판 CSS

--- hojarang_frontend/client/src/components/Common/SideTab.jsx
+++ hojarang_frontend/client/src/components/Common/SideTab.jsx
@@ -1,20 +1,47 @@
-import React, { useState } from 'react'
-import Content from './Content'
+import Highlight from '@/assets/Highlight.png'
+
+const activetabStyles = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundImage: `url(${Highlight})`, // 배경 이미지 경로 설정
+  backgroundSize: 'cover',
+  padding: '10px',
+  cursor: 'pointer',
+  transition: 'background-color 0.3s',
+  fontWeight : "900"
+};
+const tabStyles = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '10px',
+  cursor: 'pointer',
+  transition: 'background-color 0.3s',
+  color : "#888888",
+  fontWeight : "bold"
+}
+
 
 export default function SideTab({ tabs, activeTabIndex, setActiveTabIndex }){
 
   return (
-    <div className='space-x-14 mx-auto'>
-      <ul className="space-y-10 my-10">
+    <div className='flex space-x-14 mx-auto'>
+      <ul className="space-y-10 my-10 mx-auto">
         {tabs.map((tab, index) => (
           <li
             key={index}
             onClick={() => setActiveTabIndex(index)}
-            className="m-auto cursor-pointer"
+            className='w-32 mx-auto'
+            style={
+              activeTabIndex === index
+                ? { ...activetabStyles, fontWeight: 'bold' }
+                : tabStyles
+            }
           >
-            <div className={`${activeTabIndex === index ? 'font-bold' : ''}`}>
-              {tab.title}
-            </div>
+            {tab.title}
           </li>
         ))}
       </ul>
