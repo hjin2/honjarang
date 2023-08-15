ca891f59980967eb8a1e4cd861a75be9cf6b3fc8
jaewook <jaewook71828@gmail.com>
2023-08-15 16:27:16 +0900

    Design : 채팅 디자인 수정

--- hojarang_frontend/client/src/components/Chatting/ChatList.jsx
+++ hojarang_frontend/client/src/components/Chatting/ChatList.jsx
@@ -4,23 +4,79 @@ import { useNavigate } from 'react-router';
 
 
 function ChatList({list}) {
+  const [Diff, setDiff] = useState('')
+  const [msg, setmsg] = useState('')
   const navigate = useNavigate()
   const onClick = () => {
     navigate(`./${list.id}`)
   }
 
+  
+  const Diff_cal = () => {
+  let today = new Date();   
+  let year = today.getFullYear(); // 년도
+  let month = today.getMonth() + 1;  // 월
+  let date = today.getDate();  // 날짜
+  let day = today.getTime();  // 요일
+
+  if (list.last_message_created_at !== null) {
+   let last_time = new Date(list.last_message_created_at)
+    let year2 = last_time.getFullYear()
+    let month2 = last_time.getMonth() + 1
+    let date2 = last_time.getDate()
+    let day2 = last_time.getTime()
+
+    if (year - year2 !== 0) {
+      setDiff(year - year2)
+      setmsg('년 전')
+    }
+    else if (month - month2 !== 0) {
+      setDiff(month - month2)
+      setmsg('달 전')
+
+    }
+    else if (date - date2 !== 0) {
+      setDiff(date - date2)
+      setmsg('일 전')
+    }
+    else if (day - day2 !== 0) {
+  
+      if (day - day2 >= 60*60*1000) {
+        const diff = day - day2
+        setDiff(Math.floor(diff / (60*60*1000)))
+        setmsg('시간 전')
+      }
+      else {
+        const diff = day - day2
+        setDiff(Math.floor(diff / (60*1000)))
+        setmsg('분 전')
+      }
+
+    }
+
+  }
+}
+
+useEffect(() => {
+  Diff_cal()
+},[])
 
   
   return (
-    <div  className="border-b border-solid border-black" onClick={onClick}>
+    <div  className="border-b border-solid border-gray-300 cursor-pointer m-2" onClick={onClick}>
       <div className='grid-raw-10'>
-      <span>{list.name}</span> <span>{list.participant_count}</span>
+      <span>{list.name}</span> 
+      {list.participant_count === 2 ? null : <span className='text-xs mb-2 text-gray2 ml-2'>{list.participant_count}</span>}
       <br />
-      {list.last_message==='메시지 없음' ? <span>아직 작성된 대화가 없습니다.</span> :
-      <span>{list.last_message}</span> 
+      {list.last_message_created_at===null ? <span className='text-sm text-gray1'>아직 작성된 대화가 없습니다.</span> :
+      <span className='text-sm'>{list.last_message}</span>
       }
+      {list.last_message_created_at===null ? null :
+      <span className='text-xs ml-2 text-gray1'>{Diff}{msg}</span>
+      }
+      {list.unread_message_count === 0 ? null : <div className='w-5 h-5 text-center bg-red-400 mr-auto float-right text-white rounded'>{list.unread_message_count}</div>}
+      
       </div>
-      <div className='grid-raw-2'>{list.unread_message_count}</div>
     </div>
   )
 }
