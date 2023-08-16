c83fec2bf091281b0f9dff66f04bb12676780caf
jaewook <jaewook71828@gmail.com>
2023-08-16 12:29:29 +0900

    WebRTC_채팅 수정

--- hojarang_frontend/client/src/main.jsx
+++ hojarang_frontend/client/src/main.jsx
@@ -7,9 +7,9 @@ import store from './redux/store.jsx';
 import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
+  // </React.StrictMode>
 );
