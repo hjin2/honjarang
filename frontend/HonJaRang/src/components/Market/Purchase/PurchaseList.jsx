d286fbe27d6ae409b9700d6c80a9478054313a06
김동혁 <fhg4561@naver.com>
2023-08-17 12:52:28 +0900

    Fix : 로그인 후 접속페이지 수정

--- frontend/HonJaRang/src/components/Market/Purchase/PurchaseList.jsx
+++ frontend/HonJaRang/src/components/Market/Purchase/PurchaseList.jsx
@@ -7,6 +7,13 @@ import "../pagination.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { API } from "@/apis/config"
+import { initializeApp } from 'firebase/app';
+import {
+  getMessaging,
+  getToken,
+  onMessage,
+} from 'firebase/messaging';
+
 
 export default function PurchaseList() {
   const token = localStorage.getItem('access_token')
@@ -53,6 +60,78 @@ export default function PurchaseList() {
   const handleKeyword = (e) =>{
     setKeyword(e.target.value)
   }
+
+  const firebaseConfig = {
+    apiKey: import.meta.env.VITE_APP_FIREBASE_APIKEY,
+    authDomain: import.meta.env.VITE_APP_FIREBASE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_APP_FIREBASE_PROJECTID,
+    storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APP_FIREBASE_APPID,
+    measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENTID
+  };
+  const app = initializeApp(firebaseConfig);
+  const messaging = getMessaging(app);
+  useEffect(() => {
+    requestPermission();
+    getToken(messaging, {
+      vapidKey: import.meta.env.VITE_APP_FIREBASE_VAPIDKEY,
+    })
+      .then((currentToken) => {
+        if (currentToken) {
+          localStorage.setItem('fcm_token', currentToken)
+          Push(currentToken)
+        } else {
+          console.log('No registration token available. Request permission to generate one.');
+        }
+      })
+      .catch((err) => {
+        console.log('An error occurred while retrieving token. ', err);
+      });
+
+    onMessage(messaging, (payload) => {
+      console.log('Message received. ', payload);
+      const { title, body } = payload.notification;
+      const options = {
+        body,
+        icon: '/firebase-logo.png',
+      };
+      new Notification(title, options);
+    });
+  }, []);
+
+  const requestPermission = () => {
+    console.log('Requesting permission...');
+    Notification.requestPermission().then((permission) => {
+      if (permission === 'granted') {
+        console.log('Notification permission granted.');
+      } else {
+        console.log('Unable to get permission to notify.');
+      }
+    });
+  };
+
+
+  const Push = (currentToken) => {
+    const token = currentToken
+    const access_token = localStorage.getItem('access_token')
+    axios.post(`${API.USER}/fcm-token`,
+    {fcm_token : token},
+    {headers: {
+      'Authorization' : `Bearer ${access_token}`
+    }})
+    .then((res) => {
+      console.log(res)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+
+
+
+
+
   return (
     <div className="h-full">
       <div className="flex justify-end mb-5">
